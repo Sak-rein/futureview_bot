5fd016889483e3381b0fa1b33b1535bebd107b8a
Sak-rein <starskky324@gmail.com>
2026-06-14 01:50:32 +0800

    flask thread

--- main.py
+++ main.py
@@ -6,8 +6,22 @@ import asyncio
 from dotenv import load_dotenv
 load_dotenv()
 
+from flask import Flask
 from threading import Thread
 from discord.ext import commands
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive"
+
+def run_web():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
+# 啟動網頁伺服器（Keep Alive）
+Thread(target=run_web, daemon=True).start()
 
 # 使用者安裝型（全域），伺服器變數放著以防萬一
 GUILD_ID = 728929244830498857
